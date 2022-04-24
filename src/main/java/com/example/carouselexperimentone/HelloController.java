c57ec38c00d39536ea445b898b914ff8623371ec
sherrmann <samuelherrmann@gmx.net>
2022-04-24 15:35:13 +0100

    display an image in tab1

--- src/main/java/com/example/carouselexperimentone/HelloController.java
+++ src/main/java/com/example/carouselexperimentone/HelloController.java
@@ -2,13 +2,33 @@ package com.example.carouselexperimentone;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
-public class HelloController {
+public class HelloController{
     @FXML
-    private Label welcomeText;
+    TabPane tabPane;
+    @FXML
+    Tab tab1;
+    @FXML
+    AnchorPane tab1Pane;
+    @FXML
+    ImageView imageViewTab1;
+    @FXML
+    Tab tab2;
+    @FXML
+    AnchorPane tab2Pane;
+    @FXML
+    ImageView imageViewTab2;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        Image imageTab1 = new Image(getClass().getResourceAsStream("CollectionsCheatSheet.png"));
+        imageViewTab1.fitWidthProperty().bind(tab1Pane.widthProperty());
+        imageViewTab1.setImage(imageTab1);
     }
+
 }

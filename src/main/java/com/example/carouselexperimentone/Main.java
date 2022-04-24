83b87dca7b3fcf662e7425e7a5d2af65841a2239
sherrmann <samuelherrmann@gmx.net>
2022-04-24 16:32:44 +0100

    messed around with sizes and fxml attributes

--- src/main/java/com/example/carouselexperimentone/Main.java
+++ src/main/java/com/example/carouselexperimentone/Main.java
@@ -7,13 +7,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("tabLayout.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("DisplayCarousel");
         stage.setScene(scene);
+        stage.setWidth(1600.0);
+        stage.setHeight(800.0);
         stage.show();
     }
 
--- src/main/java/com/example/carouselexperimentone/carouselController.java
+++ src/main/java/com/example/carouselexperimentone/carouselController.java
@@ -1,14 +1,13 @@
 package com.example.carouselexperimentone;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
-public class HelloController{
+public class carouselController {
     @FXML
     TabPane tabPane;
     @FXML
@@ -26,9 +25,13 @@ public class HelloController{
 
     @FXML
     public void initialize() {
-        Image imageTab1 = new Image(getClass().getResourceAsStream("CollectionsCheatSheet.png"));
-        imageViewTab1.fitWidthProperty().bind(tab1Pane.widthProperty());
-        imageViewTab1.setImage(imageTab1);
+        try {
+            Image imageTab1 = new Image(getClass().getResourceAsStream("CollectionsCheatSheet.png"));
+            imageViewTab1.setImage(imageTab1);
+        }
+        catch(NullPointerException e){
+            e.printStackTrace();
+        }
     }
 
 }

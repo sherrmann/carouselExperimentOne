4eedf9c1edee786c23fc718e83e1c65632477cfc
sherrmann <samuelherrmann@gmx.net>
2022-04-26 17:11:27 +0100

    add comments

--- src/main/java/com/example/carouselexperimentone/Main.java
+++ src/main/java/com/example/carouselexperimentone/Main.java
@@ -7,18 +7,16 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
         ImageView imageView = new ImageView(new Image(getClass().getResourceAsStream("CollectionsCheatSheet.png")));
         imageView.setPreserveRatio(true);
         AnchorPane anchorPane = new AnchorPane(imageView);
         Scene scene = new Scene(anchorPane);
         stage.setScene(scene);
-        imageView.fitHeightProperty().bind(anchorPane.heightProperty());
-        imageView.fitWidthProperty().bind(anchorPane.widthProperty());
+        imageView.fitHeightProperty().bind(anchorPane.heightProperty()); // bind(scene... works as well
+        imageView.fitWidthProperty().bind(anchorPane.widthProperty());  // bind(stage... cuts parts of the image off
         stage.show();
     }
 

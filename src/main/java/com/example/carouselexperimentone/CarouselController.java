53c823c22ae4e0a694cc1b656da7b3ae1181b76d
sherrmann <samuelherrmann@gmx.net>
2022-04-27 16:49:30 +0100

    moved stuff from main into controller, load fxml // TODO: add menu

--- src/main/java/com/example/carouselexperimentone/CarouselController.java
+++ src/main/java/com/example/carouselexperimentone/CarouselController.java
@@ -1,35 +1,44 @@
 package com.example.carouselexperimentone;
 
+import javafx.fxml.FXML;
+import javafx.geometry.Side;
 import javafx.scene.Parent;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 
 public class CarouselController {
-    AnchorPane anchorPane;
+    @FXML
+    TabPane tabPane;
+    @FXML
+    Tab defaultTab;
+    @FXML
+    VBox defaultVBox;
+    ImageViewPane imageViewPane;
     ImageView imageView;
     Image image;
     URL url;
-    public Parent createContent() {
+
+    public void initialize() {
         try {
             url = getClass().getResource("CollectionsCheatSheet.png");
-            image = new Image(String.valueOf(url));
+            image = new Image(url.toString());
             imageView = new ImageView(image);
-            anchorPane = new AnchorPane(imageView);
+            imageView.setPreserveRatio(true);
+            imageViewPane = new ImageViewPane(imageView);
+            VBox.setVgrow(imageViewPane, Priority.ALWAYS);
+            defaultVBox.getChildren().add(imageViewPane);
+            tabPane.setSide(Side.LEFT);
 
-
-
-//            Image imageTab1 = new Image(getClass().getResourceAsStream("CollectionsCheatSheet.png"));
-//            imageViewTab1.setImage(imageTab1);
-//            imageViewTab1.fitWidthProperty().bind(tabPane.heightProperty());
-        }
-        catch(NullPointerException e){
+        } catch (NullPointerException e) {
             e.printStackTrace();
         }
-
-        return anchorPane;
     }
 
 }
--- src/main/java/com/example/carouselexperimentone/Main.java
+++ src/main/java/com/example/carouselexperimentone/Main.java
@@ -1,7 +1,9 @@
 package com.example.carouselexperimentone;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Side;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
@@ -13,6 +15,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /* Thank you, thank you, thank you!!!
  https://stackoverflow.com/questions/48804283/javafx-imageview-fits-container
@@ -34,26 +38,31 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) {
-        Image image = new Image(getClass().getResourceAsStream("CollectionsCheatSheet.png"));
-        ImageView imageView = new ImageView(image);
-        imageView.setPreserveRatio(true);
-        ImageViewPane imageViewPane = new ImageViewPane(imageView);
-        Button button = new Button("left");
-        Button button2 = new Button("change Image");
-        Button button3 = new Button("add Image");
-        Button button4 = new Button("right");
-        HBox bottomMenu = new HBox(button, button2, button3, button4);
-        VBox vBox = new VBox(imageViewPane, bottomMenu);
-        VBox.setVgrow(imageViewPane, Priority.ALWAYS);
-        Tab tab1 = new Tab("Java", vBox);
-        Tab tab2 = new Tab("Git", new AnchorPane());
-        Tab tab3 = new Tab("IntelliJ", new HBox());
-        TabPane tabPane = new TabPane(tab1, tab2, tab3);
-        tabPane.setSide(Side.LEFT);
-        Scene scene = new Scene(tabPane);
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent parent = fxmlLoader.load(getClass().getResource("tabLayout.fxml"));
+        Scene scene = new Scene(parent);
         stage.setScene(scene);
         stage.show();
+//        Image image = new Image(getClass().getResourceAsStream("CollectionsCheatSheet.png"));
+//        ImageView imageView = new ImageView(image);
+//        imageView.setPreserveRatio(true);
+//        ImageViewPane imageViewPane = new ImageViewPane(imageView);
+//        Button button = new Button("left");
+//        Button button2 = new Button("change Image");
+//        Button button3 = new Button("add Image");
+//        Button button4 = new Button("right");
+//        HBox bottomMenu = new HBox(button, button2, button3, button4);
+//        VBox vBox = new VBox(imageViewPane, bottomMenu);
+//        VBox.setVgrow(imageViewPane, Priority.ALWAYS);
+//        Tab tab1 = new Tab("Java", vBox);
+//        Tab tab2 = new Tab("Git", new AnchorPane());
+//        Tab tab3 = new Tab("IntelliJ", new HBox());
+//        TabPane tabPane = new TabPane(tab1, tab2, tab3);
+//        tabPane.setSide(Side.LEFT);
+//        Scene scene = new Scene(tabPane);
+//        stage.setScene(scene);
+//        stage.show();
     }
 
     public static void main(String[] args) {

c979d377514a6d7d976ebddb9cd4d43850282495
sherrmann <samuelherrmann@gmx.net>
2022-04-27 12:44:22 +0100

    bug: bottemMenubar increases its height

--- src/main/java/com/example/carouselexperimentone/Main.java
+++ src/main/java/com/example/carouselexperimentone/Main.java
@@ -2,11 +2,13 @@ package com.example.carouselexperimentone;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /* Thank you, thank you, thank you!!!
@@ -34,7 +36,14 @@ public class Main extends Application {
         ImageView imageView = new ImageView(image);
         imageView.setPreserveRatio(true);
         ImageViewPane imageViewPane = new ImageViewPane(imageView);
-        Tab tab1 = new Tab("Java", imageViewPane);
+        Button button = new Button("Hello");
+        Button button2 = new Button("Menu");
+        HBox bottomMenu = new HBox(button2);
+        VBox vBox = new VBox(button, imageViewPane, bottomMenu);
+        VBox.setVgrow(bottomMenu, Priority.ALWAYS);
+        System.out.println(VBox.getVgrow(bottomMenu));
+        bottomMenu.setBackground(new Background(new BackgroundFill(Color.YELLOW, null, null)));
+        Tab tab1 = new Tab("Java", vBox);
         TabPane tabPane = new TabPane(tab1);
         Scene scene = new Scene(tabPane);
         stage.setScene(scene);

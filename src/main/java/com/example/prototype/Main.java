66008295d4d89c7200ddb90bf6d56e9acd9a1587
Samuel Herrmann <samuelherrmann@gmx.net>
2022-07-13 14:47:27 +0100

    add window icon

--- src/main/java/com/example/prototype/Main.java
+++ src/main/java/com/example/prototype/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -33,6 +34,7 @@ public class Main extends Application {
         Scene scene = new Scene(parent);
         stage.setScene(scene);
         stage.setTitle("Document Carousel Prototype");
+        stage.getIcons().add( new Image(getClass().getResource("icon.png").toString()) );
         stage.show();
     }
 
